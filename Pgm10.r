aa56fe966b6ad0dfcd2b735e13f98986ea4d494a
MuttuS111 <muttusy3@gmail.com>
2025-06-29 08:21:38 +0530

    Update Pgm10.r

--- Pgm10.r
+++ Pgm10.r
@@ -1,15 +1,27 @@
+# Load the mtcars dataset
 cat("mtcars data set is loaded\n")
-mtcars
-inpu<-mtcars[c("mpg","wt","cyl")]
-x<-input[1:32,]
-a<-lm(formula=mpg~wt+cyl,x)
-cat("determining cofficient of regression model/n")
-a
-y<-mtcars[c("wt","cyl")]
-nd<-y[1:5,]
-cat("values to be predicted\n")
-nd
-result<-predict(a,nd)
-cat("comparre\n")
-compare<-data.frame(prdicted=result,actual=mtcars$mpg[1:5])
-compare
+data(mtcars)  # ensures the dataset is available
+print(mtcars)
+
+# Select relevant columns
+input <- mtcars[c("mpg", "wt", "cyl")]
+x <- input[1:32, ]
+
+# Fit linear regression model
+a <- lm(formula = mpg ~ wt + cyl, data = x)
+cat("Determining coefficients of regression model\n")
+print(a)
+
+# New data to predict
+y <- mtcars[c("wt", "cyl")]
+nd <- y[1:5, ]
+cat("Values to be predicted\n")
+print(nd)
+
+# Predict using the model
+result <- predict(a, nd)
+
+# Compare predicted and actual values
+cat("Compare\n")
+compare <- data.frame(predicted = result, actual = mtcars$mpg[1:5])
+print(compare)
