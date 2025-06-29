00f08cf2209884b060ab83d0b6ff640f5b1607e3
MuttuS111 <muttusy3@gmail.com>
2025-06-29 09:55:17 +0530

    Create Pgm2.r

---
+++ Pgm2.r
@@ -0,0 +1,23 @@
+#variables
+name="ABC"
+age=21
+is_student=TRUE
+cat("Name:",name)
+cat("Age:",age)
+cat("Student:",is_student)
+
+#constants
+pi=3.142
+e=2.782
+cat("pi value:",pi)
+cat("e value:",e)
+
+#datatypes
+integer=10L
+float=10.6
+char_str="Hello"
+logical_val=TRUE
+cat("Integer value:",integer)
+cat("Float value:",float)
+cat("Character string:",char_str)
+cat("Boolean value:",logical_val)
